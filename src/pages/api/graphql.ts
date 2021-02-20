9aa07ceca73e5b162d0014522a8ecce5cf54fcf3
mikohan <angara99@gmail.com>
2021-02-20 13:08:42 +0300

    Babelrc graphql added

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,19 +1,29 @@
 import { ApolloServer, gql } from 'apollo-server-micro';
 import connectDb from '../../lib/mongoose';
+import { mergeResolvers, mergeTypeDefs } from 'graphql-toolkit';
+import { habitsResolvers } from '~/pages/api/habits/resolvers';
+import { habitsMutations } from '~/pages/api/habits/mutations';
+import Habits from '~/pages/api/habits/habits.graphql';
 
-const typeDefs = gql`
+const fakeTypeDefs = gql`
   type Query {
     sayHello: String!
   }
 `;
+const typeDefs = mergeTypeDefs([fakeTypeDefs, Habits]);
 
-const resolvers = {
+const fakeResolvers = {
   Query: {
     sayHello: () => {
       return 'Hello world!';
     },
   },
 };
+const resolvers = mergeResolvers([
+  fakeResolvers,
+  habitsResolvers,
+  habitsMutations,
+]);
 
 const apolloServer = new ApolloServer({
   typeDefs,
