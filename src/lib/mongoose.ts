20364f28133e51aaed437ffb787867b7b5c4cb9c
mikohan <angara99@gmail.com>
2021-02-20 12:11:16 +0300

    Connection not working

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -1,2 +1,21 @@
-export const some = 'some';
-console.log(process.env.MONGO_URL);
+import mongoose from 'mongoose';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const connectDb = (handler: any) => async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  if (mongoose.connections[0].readyState !== 1) {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+    });
+  }
+  return handler(req, res);
+};
+
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log('Connected to mongo');
+});
+
+export default connectDb;
